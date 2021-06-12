c32669d1bcb6c9af345d248a37f9dcd564179fc5
RaphaelChevallier <raphaelchevallier@hotmail.com>
2021-06-11 22:15:32 -0700

    contact page

--- personal-website/pages/contact.tsx
+++ personal-website/pages/contact.tsx
@@ -1,8 +1,47 @@
 import React from "react"
+import { useState } from 'react';
 import Layout from "../layouts/main_layout"
+import styles from "../styles/contact.module.css"
 
 const Contact = () => {
-  return <Layout>Contact Info Content</Layout>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleEmailContact = e => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+    };
+    console.log(data);
+  };
+
+  return (
+    <Layout>
+      <form onSubmit={handleEmailContact} className={styles.form}>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            onChange={e => setName(e.target.value)}
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            onChange={e => setMessage(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
+    </Layout>
+  )
 }
 
 export default Contact
